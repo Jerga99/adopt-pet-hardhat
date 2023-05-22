b810622b9517f58b89ac411bd4b59e0fdf608426
Filip Jerga <jerga99@gmail.com>
2023-05-22 08:53:19 -0600

    Fixtures

--- test/PetAdoption.js
+++ test/PetAdoption.js
@@ -1,9 +1,10 @@
 
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
 
 describe("PetAdoption", function() {
 
-  async function deployContract() {
+  async function deployContractFixture() {
     const [owner] = await ethers.getSigners();
     const PetAdoption = await ethers.getContractFactory("PetAdoption");
     const contract = await PetAdoption.deploy();
@@ -13,16 +14,14 @@ describe("PetAdoption", function() {
 
   describe("Deployment", function() {
     it("Should set the right owner", async function() {
-      const { owner, contract } = await deployContract();
+      const { owner, contract } = await loadFixture(deployContractFixture);
       const contractOwner = await contract.owner();
-
       expect(contractOwner).to.equal(owner.address);
     });
 
     it("Should return the right owner", async function() {
-      const { owner, contract } = await deployContract();
+      const { owner, contract } = await loadFixture(deployContractFixture);
       const contractOwner = await contract.getOwner();
-
       expect(contractOwner).to.equal(owner.address);
     });
   });
