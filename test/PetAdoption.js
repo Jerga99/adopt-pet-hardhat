06ff6c24de4667c29a628b2019f4d13b98149a61
Filip Jerga <jerga99@gmail.com>
2023-05-22 08:06:03 -0600

    First test

---
+++ test/PetAdoption.js
@@ -0,0 +1,14 @@
+
+const { expect } = require("chai");
+
+describe("PetAdoption", function() {
+
+  describe("Deployment", function() {
+    it("Should set the right owner", function() {
+      expect(true).to.equal(true);
+    });
+  });
+});
+
+
+// npx hardhat test --network hardhat
