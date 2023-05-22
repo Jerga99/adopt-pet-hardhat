46733434031f6a4a93552a051e31438f9e5c5654
Filip Jerga <jerga99@gmail.com>
2023-05-22 08:28:26 -0600

    First real test

--- test/PetAdoption.js
+++ test/PetAdoption.js
@@ -3,9 +3,20 @@ const { expect } = require("chai");
 
 describe("PetAdoption", function() {
 
+  async function deployContract() {
+    const [owner] = await ethers.getSigners();
+    const PetAdoption = await ethers.getContractFactory("PetAdoption");
+    const contract = await PetAdoption.deploy();
+
+    return { owner, contract };
+  }
+
   describe("Deployment", function() {
-    it("Should set the right owner", function() {
-      expect(true).to.equal(true);
+    it("Should set the right owner", async function() {
+      const { owner, contract } = await deployContract();
+      const contractOwner = await contract.owner();
+
+      expect(contractOwner).to.equal(owner.address);
     });
   });
 });
