f1266a821671d9af3ae76d54b7438cf0fef55eb0
Filip Jerga <jerga99@gmail.com>
2023-05-23 14:31:16 -0600

    Adopt pet function

--- contracts/PetAdoption.sol
+++ contracts/PetAdoption.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.9;
 contract PetAdoption {
   address public owner;
   uint public petIndex = 0;
+  uint[] public allAdoptedPets;
+
+  mapping(uint => address) public petIdxToOwnerAddress;
+  mapping(address => uint[]) public ownerAddressToPetList;
 
   constructor(uint initialPetIndex) {
     owner = msg.sender;
@@ -13,6 +17,15 @@ contract PetAdoption {
   function addPet() public {
     require(msg.sender == owner, "Only a contract owner can add a new pet!");
     petIndex++;
+  }
+
+  function adoptPet(uint adoptIdx) public {
+    require(adoptIdx < petIndex, "Pet index out of bound!");
+    require(petIdxToOwnerAddress[adoptIdx] == address(0), "Pet is already adopted");
+
+    petIdxToOwnerAddress[adoptIdx] = msg.sender;
+    ownerAddressToPetList[msg.sender].push(adoptIdx);
+    allAdoptedPets.push(adoptIdx);
   }
 
   function getOwner() public view returns(address) {
