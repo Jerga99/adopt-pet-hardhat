ab2186aca841fa2a18c878892cca9483c8d92dd1
Filip Jerga <jerga99@gmail.com>
2023-05-24 08:42:23 -0600

    Base layout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Dapp from './Dapp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Dapp />
   </React.StrictMode>
 );
 
