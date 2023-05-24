d501f4420631f760facc7dc773f4844111123074
Filip Jerga <jerga99@gmail.com>
2023-05-24 08:55:31 -0600

    Get all pets and set them

--- frontend/src/Dapp.js
+++ frontend/src/Dapp.js
@@ -1,13 +1,28 @@
+
+import { useState, useEffect } from "react";
 import { Navbar } from "./components/Navbar";
 import { PetItem } from "./components/PetItem";
 import { TxError } from "./components/TxError";
 
 function Dapp() {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    async function fetchPets() {
+      const res = await fetch("/pets.json");
+      const data = await res.json();
+      setPets(data);
+    } 
+
+    fetchPets();
+  }, []);
+
   return (
     <div className="container">
       <TxError />
       <br />
       <Navbar />
+      {JSON.stringify(pets)}
       <div className="items">
         <PetItem />
       </div>
