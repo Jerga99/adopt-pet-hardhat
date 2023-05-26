82c1fbfcbf39d628045635167c355679dd2bf790
Filip Jerga <jerga99@gmail.com>
2023-05-26 08:00:24 -0600

    Save artifacts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,7 +14,31 @@ async function main() {
   const PetAdoption = await hre.ethers.getContractFactory("PetAdoption");
   const contract = await PetAdoption.deploy(PETS_COUNT);
 
+  await contract.deployed();
+
   console.log(`PetAdoption deployed to ${contract.address}`);
+
+  saveContractFiles(contract);
+}
+
+function saveContractFiles(contract) {
+  const contractDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+
+  if (!fs.existsSync(contractDir)) {
+    fs.mkdirSync(contractDir);
+  }
+
+  fs.writeFileSync(
+    path.join(contractDir, `contract-address-${network.name}.json`),
+    JSON.stringify({PetAdoption: contract.address}, null, 2)
+  );
+
+  const PetAdoptionArtifact = artifacts.readArtifactSync("PetAdoption");
+
+  fs.writeFileSync(
+    path.join(contractDir, "PetAdoption.json"),
+    JSON.stringify(PetAdoptionArtifact, null, 2)
+  );
 }
 
 main().catch(error => {
