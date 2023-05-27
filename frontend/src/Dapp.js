b23169b71ee997b6791c71ae042e4f0d854bb439
Filip Jerga <jerga99@gmail.com>
2023-05-27 13:27:29 -0600

    Init contract

--- frontend/src/Dapp.js
+++ frontend/src/Dapp.js
@@ -6,11 +6,16 @@ import { TxError } from "./components/TxError";
 import { WalletNotDetected } from "./components/WalletNotDetected";
 import { ConnectWallet } from "./components/ConnectWallet";
 
+import { ethers } from "ethers";
+import contractAddress from "./contracts/contract-address-localhost.json";
+import PetAdoptionArtifact from "./contracts/PetAdoption.json";
+
 const HARDHAT_NETWORK_ID = Number(process.env.REACT_APP_NETWORK_ID);
 
 function Dapp() {
   const [pets, setPets] = useState([]);
   const [selectedAddress, setSelectedAddress] = useState(undefined);
+  const [contract, setContract] = useState(undefined);
 
   useEffect(() => {
     async function fetchPets() {
@@ -46,10 +51,20 @@ function Dapp() {
   async function initiliazeDapp(address) {
     setSelectedAddress(address);
     const contract = await initContract();
+
+    console.log(contract);
   }
 
   async function initContract() {
-    alert("I should init the contract!");
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const contract = new ethers.Contract(
+      contractAddress.PetAdoption,
+      PetAdoptionArtifact.abi,
+      await provider.getSigner(0)
+    );
+
+    setContract(contract);
+    return contract;
   }
 
   async function switchNetwork() {
