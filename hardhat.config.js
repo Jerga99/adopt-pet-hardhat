3293697a4edbcfc823afc8da0ff544f22cdd6ea5
Filip Jerga <jerga99@gmail.com>
2023-05-28 13:31:37 -0600

    Deploy to Polygon

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,29 @@
 
+const keys = require("./keys.json");
+
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
   networks: {
-    localhost: {}
+    localhost: {},
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [keys.DEPLOY_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: keys.POLYGON_API_KEY
+  },
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
   }
 };
